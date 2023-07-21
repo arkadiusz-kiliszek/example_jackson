8ff92fa6029c2246881d529452c08b8aa8958615
Arkadiusz Kiliszek <arkadiusz.kiliszek@gmail.com>
2023-07-21 14:02:42 +0200

    initial

---
+++ src/main/java/com/example/examplejackson/ExampleJacksonApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplejackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJacksonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleJacksonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examplejackson/ExampleJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplejackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleJacksonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
